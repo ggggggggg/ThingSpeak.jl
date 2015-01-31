f7e49262861556588fafe625348426887c94fd03
Galen O'Neil <galen.oneil@nist.gov>
2015-01-31 13:40:19 -0700

    initial commit

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,7 @@ using ThingSpeak
 using Base.Test
 
 # write your own tests here
-@test 1 == 1
+using ThingSpeak
+ThingSpeak.setdefaultchannel("YBOH2D5UMGPJ50LB") #this is technically a secret but its a dedicated channel for testing
+response = tsupdate(1,2,3,status="ran runtests.jl for ThingSpeak.jl")
+tssuccess(response)
