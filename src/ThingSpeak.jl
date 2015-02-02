60525548fe6fe498ada3db80e3307a36a34898d2
Galen O'Neil <galen.oneil@nist.gov>
2015-02-02 09:28:21 -0700

    update testing and api for only one field

--- src/ThingSpeak.jl
+++ src/ThingSpeak.jl
@@ -8,10 +8,10 @@ const updateheaders = {"Content-type"=> "application/x-www-form-urlencoded","Acc
 defaultchannel = TSChannel("")
 setdefaultchannel(s::ASCIIString) = (global defaultchannel = TSChannel(s))
 
-function updatedata(channel::TSChannel, status::ASCIIString, fields...)
+function updatedata(channel::TSChannel, status::ASCIIString, fields::Dict)
   data = "key=$(channel.writekey)"
-  for (i,f) in enumerate(fields)
-    data *= "&field$i=$f"
+  for (i,f) in fields
+    data *= "&field$i="*@sprintf("%f",f)
   end
   if status != ""
     length(status)>140 && error("status is limited to 140 characters in ThingSpeak API")
@@ -19,9 +19,13 @@ function updatedata(channel::TSChannel, status::ASCIIString, fields...)
   end
   data
 end
-function tsupdate(fields::Number...;status::ASCIIString="", channel::TSChannel=defaultchannel)
-  response = post(updateapiurl, data=updatedata(channel, status, fields...), headers=updateheaders)
+function tsupdate(fields::Dict;status::ASCIIString="", channel::TSChannel=defaultchannel)
+  response = post(updateapiurl, data=updatedata(channel, status, fields), headers=updateheaders)
 end
+function tsupdate(fields...;status::ASCIIString="", channel::TSChannel=defaultchannel)
+  response = post(updateapiurl, data=updatedata(channel, status, {k=>v for (k,v) in enumerate(fields)}), headers=updateheaders)
+end
+
 tssuccess(r::Requests.Response) = r.data != "0"
 
 export tsupdate, tssuccess
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,16 +4,20 @@ using Base.Test
 # write your own tests here
 using ThingSpeak
 global apikey
-try
-  apikey = ENV["THINGSPEAK_API_KEY"]
-  println("Loading API Key from environment variable THINGSPEAK_API_KEY")
-catch
-  println("Loading API Key from testingapikey file")
-  apikey = open("testingapikey") do f
-    strip(readline(f))
+function loadsecret(name="TEST_SECRET")
+  if Base._hasenv(name) #change to "name in ENV" once in works
+    println("Loading $name from environment variable")
+    return ENV[name]
+  else
+    println("Loading $name from file")
+    return open(name) do f
+      strip(readline(f))
+    end
   end
 end
 
-ThingSpeak.setdefaultchannel(apikey) #this is technically a secret but its a dedicated channel for testing
-response = tsupdate(1,2,3,status="ran runtests.jl for ThingSpeak.jl")
+ThingSpeak.setdefaultchannel(loadsecret("THINGSPEAK_API_KEY")) #this is technically a secret but its a dedicated channel for testing
+response = tsupdate({3=>3.5},status="ran runtests.jl for ThingSpeak.jl")
+response = tsupdate(1.1,2.2)
+
 tssuccess(response)
